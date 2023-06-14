5a6ff1670edcd94d0cd087c0877624e7a8bb5384
Ignacio López Ezcurra <ignanmrk@gmail.com>
2023-06-13 22:10:56 -0300

    se agrego proyecto pooej01

---
+++ POO/POOEj01/src/Entidad/Persona.java
@@ -0,0 +1,21 @@
+
+package Entidad;
+
+
+public class Persona {
+
+ 
+    
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public String fechaNac;
+
+    public Persona() {
+    }
+    
+       public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
---
+++ POO/POOEj01/src/pooej01/POOEj01.java
@@ -0,0 +1,19 @@
+
+
+package pooej01;
+import Entidad.Persona;
+
+public class POOEj01 {
+
+  
+    public static void main(String[] args) {
+        
+        //Instanciación utilizando el constructor vacío
+        Persona primeraPersona = new Persona();
+        
+        //Instanciación utilizando el constructor por parámetro
+        Persona segundaPersona = new Persona("Mariano");
+        
+    }
+    
+}
